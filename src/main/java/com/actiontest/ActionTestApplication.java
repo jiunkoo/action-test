ad08ed86ec9e071d398ea7de58c8aff6fcccdd0e
jiunkoo <jiunkoo0106@gmail.com>
2023-10-29 21:41:01 +0900

    first commit

---
+++ src/main/java/com/actiontest/ActionTestApplication.java
@@ -0,0 +1,13 @@
+package com.actiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actiontest/ActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
