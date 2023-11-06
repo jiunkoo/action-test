3ac861381bac93bc365d70dcc00f2beacbe595c4
jiunkoo <jiunkoo0106@gmail.com>
2023-11-07 08:52:27 +0900

    deploy test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,15 @@
 REPOSITORY=/home/ec2-user/app
 cd $REPOSITORY
 
-APP_NAME=action-test #1
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+APP_NAME=action-test
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ] #2
+cp $REPOSITORY/application-env.properties $REPOSITORY/build/libs/
+
+if [ -z $CURRENT_PID ]
 then
   echo "> 현재 실행 중인 애플리케이션이 없습니다."
 else
@@ -16,5 +18,5 @@ else
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/ec2-user/app/build/libs/action-test-1.0.jar --spring.config.location=/home/ec2-user/application.yml > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_JAR_PATH > /dev/null 2> /dev/null < /dev/null &
