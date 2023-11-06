d1327b4af2e1d51f405e42bd183edb8bb46e950a
jiunkoo <jiunkoo0106@gmail.com>
2023-11-07 06:30:04 +0900

    add test properties and divide github actions workflow

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ec2-user/app
+cd $REPOSITORY
+
+APP_NAME=action-test
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 실행 중인 애플리케이션이 없습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
--- src/test/java/com/actiontest/ActionTestApplicationTests.java
+++ src/test/java/com/actiontest/ActionTestApplicationTests.java
@@ -2,8 +2,10 @@ package com.actiontest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ActionTestApplicationTests {
 
 	@Test
